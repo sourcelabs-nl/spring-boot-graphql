ddff14c4dfbcf82d4a0f60f5d207b74860027ba2
Stephan Oudmaijer <soudmaijer@bol.com>
2017-08-04 12:09:14 +0200

    Getting rid of the SchemaParserDictonary.

--- src/main/java/nl/sourcelabs/graphql/application/GraphQLApplication.kt
+++ src/main/java/nl/sourcelabs/graphql/application/GraphQLApplication.kt
@@ -1,20 +1,10 @@
 package nl.sourcelabs.graphql.application
 
-import com.coxautodev.graphql.tools.SchemaParserDictionary
-import nl.sourcelabs.graphql.application.types.Cancellation
-import nl.sourcelabs.graphql.application.types.Order
-import nl.sourcelabs.graphql.application.types.OrderItem
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class GraphQLApplication {
-
-    @Bean
-    open fun schemaParserDictionary(): SchemaParserDictionary {
-        return SchemaParserDictionary().add(dictionary = listOf(Order::class.java, OrderItem::class.java, Cancellation::class.java))
-    }
 
     companion object {
         @JvmStatic fun main(args: Array<String>) {
--- src/main/java/nl/sourcelabs/graphql/application/types/Order.kt
+++ src/main/java/nl/sourcelabs/graphql/application/types/Order.kt
@@ -1,3 +1,6 @@
 package nl.sourcelabs.graphql.application.types
 
+import graphql.annotations.GraphQLName
+
+@GraphQLName("Order")
 data class Order constructor(val orderId: String? = null, val orderReference: String? = null, val dateTimePlaced: String? = null, val totalPrice: String? = null, val orderItems: List<OrderItem>)
